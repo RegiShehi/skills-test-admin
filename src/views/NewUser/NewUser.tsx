3614ad1c834ba56e9cd3ebacfc01a19b6e4b778d
Regi Shehi <shehi.regi@gmail.com>
2021-05-29 17:59:14 +0200

    Add controlled state for user form

--- src/views/NewUser/NewUser.tsx
+++ src/views/NewUser/NewUser.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable import/no-extraneous-dependencies */
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -5,10 +6,30 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import { useHistory } from 'react-router';
+import { useState } from 'react';
 import styles from './NewUser.module.scss';
+
+interface IUserForm {
+  email: string;
+  first_name: string;
+  last_name: string;
+  birth_date: string;
+  photo_link: string;
+}
 
 const NewUser = () => {
   const history = useHistory();
+  const [formValues, setFormValues] = useState<IUserForm>({
+    email: '',
+    first_name: '',
+    last_name: '',
+    birth_date: '',
+    photo_link: '',
+  });
+
+  const handleChange = (event: any) => {
+    setFormValues((prevState) => ({ ...prevState, [event.target.name]: event.target.value }));
+  };
 
   return (
     <Container className={styles.addUserContainer}>
@@ -18,13 +39,19 @@ const NewUser = () => {
 
       <h3 className={styles.title}>Add new user</h3>
 
-      <Form>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          console.log('Form values: ', formValues);
+        }}
+      >
         <Form.Group as={Row} controlId="formHorizontalEmail">
           <Form.Label column sm={2}>
             Email
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="email" placeholder="Email" />
+            <Form.Control placeholder="Email" name="email" onChange={(e) => handleChange(e)} />
           </Col>
         </Form.Group>
 
@@ -33,7 +60,7 @@ const NewUser = () => {
             First Name
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="text" placeholder="First name" />
+            <Form.Control type="text" placeholder="First name" name="first_name" onChange={(e) => handleChange(e)} />
           </Col>
         </Form.Group>
 
@@ -42,7 +69,7 @@ const NewUser = () => {
             Last Name
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="text" placeholder="Last name" />
+            <Form.Control type="text" placeholder="Last name" name="last_name" onChange={(e) => handleChange(e)} />
           </Col>
         </Form.Group>
 
@@ -51,7 +78,7 @@ const NewUser = () => {
             Birthday
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="text" placeholder="Birthday" />
+            <Form.Control type="text" placeholder="Birthday" name="birth_date" onChange={(e) => handleChange(e)} />
           </Col>
         </Form.Group>
 
@@ -60,7 +87,7 @@ const NewUser = () => {
             Profile picture
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="text" placeholder="Link" />
+            <Form.Control type="text" placeholder="Link" name="photo_link" onChange={(e) => handleChange(e)} />
           </Col>
         </Form.Group>
 
