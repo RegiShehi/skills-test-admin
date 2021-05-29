679eb88e4aac4460ac74517c90ffefce9bc6ab29
Regi Shehi <shehi.regi@gmail.com>
2021-05-29 18:12:32 +0200

    Update react datepicker import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
-import 'react-datepicker/dist/react-datepicker.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
