b9e81d8791395031df7ffce0d1d17c461fffb533
Regi Shehi <shehi.regi@gmail.com>
2021-05-29 17:39:40 +0200

    Add new user form

--- src/views/NewUser/NewUser.tsx
+++ src/views/NewUser/NewUser.tsx
@@ -1,5 +1,77 @@
-import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import { useHistory } from 'react-router';
+import styles from './NewUser.module.scss';
 
-const NewUser = () => <div>New user</div>;
+const NewUser = () => {
+  const history = useHistory();
+
+  return (
+    <Container className={styles.addUserContainer}>
+      <Button variant="info" onClick={() => history.push('/')}>
+        Users
+      </Button>
+
+      <h3 className={styles.title}>Add new user</h3>
+
+      <Form>
+        <Form.Group as={Row} controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" placeholder="Email" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            First Name
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="First name" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            Last Name
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="Last name" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Birthday
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="Birthday" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Profile picture
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="Link" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit">Add user</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+};
 
 export default NewUser;
